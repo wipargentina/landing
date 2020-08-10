17abf5d971dc7ee3b7d2d4298f7164be15e186a5
German G Sayago <german.sayago@gmail.com>
2020-08-10 18:12:47 -0300

    se agregó texto en hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link as Scroll } from 'react-scroll';
 
 import logo from '../assets/statics/logo-feria.svg';
 
@@ -6,7 +7,27 @@ const Hero =  (props) => (
   <div className="hero">
     <div className="content bg"></div>
     <div className="content caption">
-      <img src={logo} alt="logo feria" className="logo" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <h1>Feria del Repuesto CASE IH</h1>
+            <h3>Del 17 de Agosto al 13 de Septiembre. <br/> Importantes Descuentos, financión y envío gratis a todo el pais.</h3>
+            <Scroll
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={599}
+              className="btn btn-lg btn-outline-light"
+            >
+              contactá a un asesor
+            </Scroll>
+          </div>
+          <div className="col-md-4 logo">
+            <img src={logo} alt="logo feria" className="logo" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 )
